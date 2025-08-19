b4c3cc9a1e85fed9438ccc1902441c1e1c8ae8b5
Iyanu Ajimobi <iyanuajimobi12@gmail.com>
2025-08-19 03:30:03 +0100

    Add tracking routes and implement visitor-related handlers

---
+++ api/controllers/tracking.controller.js
@@ -0,0 +1,119 @@
+import fetch from 'node-fetch';
+
+async function AddVisitor(req, res) {
+  const baseUrl = process.env.TRACKING_APP_URL;
+
+  if (req.headers['x-clientname'] === undefined) {
+    return res.status(403).json({ error: 'Not Authorized' });
+  }
+  const tokenKey = `${req.headers['x-clientname']}_NEWS_LETTER_TOKEN`;
+  const token = process.env[tokenKey];
+
+  try {
+    const response = await fetch(baseUrl + '/visitors', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      const error = await response.json();
+      console.log(`Error: ${response.status} - ${error}`);
+      throw new Error(error.detail);
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+async function VisitorsCount(req, res) {
+  //   const page = parseInt(req.query.page, 10) || 1;
+  //   const limit = parseInt(req.query.limit, 10) || 20;
+  const baseUrl = process.env.TRACKING_APP_URL;
+
+  if (req.headers['x-clientname'] === undefined) {
+    return res.status(403).json({ error: 'Not Authorized' });
+  }
+  const tokenKey = `${req.headers['x-clientname']}_NEWS_LETTER_TOKEN`;
+  const token = process.env[tokenKey];
+
+  try {
+    const response = await fetch(`${baseUrl}/visitors/count`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    if (!response.ok) throw new Error(`HTTP ${response.status}`);
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+async function UniqueVisitorsCount(req, res) {
+  const startDate = req.query.page;
+  const endDate = req.query.startDate;
+  const baseUrl = process.env.TRACKING_APP_URL;
+
+  if (req.headers['x-clientname'] === undefined) {
+    return res.status(403).json({ error: 'Not Authorized' });
+  }
+  const tokenKey = `${req.headers['x-clientname']}_NEWS_LETTER_TOKEN`;
+  const token = process.env[tokenKey];
+
+  try {
+    const params = new URLSearchParams({
+      startDate: encodeURIComponent(startDate),
+      endDate: encodeURIComponent(endDate),
+    });
+    const response = await fetch(
+      `${baseUrl}/visitors/unique/count?${params.toString()}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    if (!response.ok) throw new Error(`HTTP ${response.status}`);
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+async function UniqueVisitors(req, res) {
+  const startDate = req.query.page;
+  const endDate = req.query.startDate;
+  const baseUrl = process.env.TRACKING_APP_URL;
+
+  if (req.headers['x-clientname'] === undefined) {
+    return res.status(403).json({ error: 'Not Authorized' });
+  }
+  const tokenKey = `${req.headers['x-clientname']}_NEWS_LETTER_TOKEN`;
+  const token = process.env[tokenKey];
+
+  try {
+    const params = new URLSearchParams({
+      startDate: encodeURIComponent(startDate),
+      endDate: encodeURIComponent(endDate),
+    });
+    const response = await fetch(
+      `${baseUrl}/visitors/unique?${params.toString()}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    if (!response.ok) throw new Error(`HTTP ${response.status}`);
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+export { AddVisitor, VisitorsCount, UniqueVisitorsCount, UniqueVisitors };
--- api/router/routes.js
+++ api/router/routes.js
@@ -4,6 +4,12 @@ import ECSVHandler from '../controllers/export-csv.controller.js';
 import RenewTokenHandler from '../controllers/renew-token.controller.js';
 import SubscriberHandler from '../controllers/subscribers.controller.js';
 import CreateSubscriberHandler from '../controllers/create-subscribers.controller.js';
+import {
+  VisitorsCount,
+  UniqueVisitors,
+  UniqueVisitorsCount,
+  AddVisitor,
+} from '../controllers/tracking.controller.js';
 
 const router = express.Router();
 
@@ -12,8 +18,12 @@ const router = express.Router();
 router.get('/client', clientHandler);
 router.get('/export-csv', ECSVHandler);
 router.get('/subscribers', SubscriberHandler);
+router.get('/tracking/visitors/count', VisitorsCount);
+router.get('/tracking/visitors/unique', UniqueVisitors);
+router.get('/tracking/visitors/unique/count', UniqueVisitorsCount);
 
 // POST routes
+router.post('/tracking/visitors', AddVisitor);
 router.post('/renew-token', RenewTokenHandler);
 router.post('/subscribers', CreateSubscriberHandler);
 
